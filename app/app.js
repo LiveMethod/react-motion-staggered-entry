75d944c3f6ac5c95b465c84ff3ea18bdbaec87e4
Rick Steele <rick@karmiclabs.com>
2015-12-04 16:49:10 -0800

    slices are moved by motion defaults, but arent actually in motion yet

--- app/app.js
+++ app/app.js
@@ -6,15 +6,53 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Motion, spring} from 'react-motion';
+import {Motion, StaggeredMotion, spring} from 'react-motion';
 
+
+const Slice = React.createClass({
+	render: function(){
+
+    var sliceStyle = {
+      display: 'block',
+      position: 'relative',
+      color: 'red', 
+      background: 'yellow',
+      border: '1px solid red',
+      width: '100%',
+      height: '100px'
+    }
+
+    // give this slice the styles passed in when its called by the app.
+    // this is a very hacky handling that will override default styles,
+    // might cause collisions, and other stuff.
+    // Improve it if you want to do anything serious.
+    for (var key in this.props.style){sliceStyle[key] = this.props.style[key]}
+		return(<div style={sliceStyle}>
+            {this.props.children}
+  				</div>)
+	}
+});
 
 const App = React.createClass ({
 	render: function(){
 		const message = "hello world";
 
 		return(
-			<h1>{message}</h1>
+			<StaggeredMotion
+        defaultStyles={[{x: 0}, {x: 10}, {x: 20}]}
+        styles={prevStyles => prevStyles.map((_, i) => {
+          return i === 0
+            ? {x: 40}
+            : prevStyles[i - 1];
+        })}>
+        {interpolatedStyles =>
+          <div>
+            {interpolatedStyles.map((style, i) =>
+              <Slice key={i} style={{left: style.x}} />
+            )}
+          </div>
+        }
+      </StaggeredMotion>
 		)
 	}
 });
